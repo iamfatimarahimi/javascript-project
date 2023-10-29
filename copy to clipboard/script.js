826fd9645c04bfa7ab35671dfcf43255ca14810a
fatima rahimi <iamfatimarahimi@gmail.com>
2023-10-29 21:18:29 +0330

    Setting a time out

--- copy to clipboard/script.js
+++ copy to clipboard/script.js
@@ -3,4 +3,11 @@ const textarea = document.querySelector('textarea');
 
 copyBtn.addEventListener('click', () =>{
     textarea.select()
+    let textValue = textarea.value;
+    navigator.clipboard.writeText(textValue)
+    copyBtn.innerText = "Text Copied!";
+    setTimeout(()=>{
+        copyBtn.innerText = "Copy";
+        copyBtn.style.background = '#03A9F4';
+    },1500)
 })
