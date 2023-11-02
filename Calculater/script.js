2ec63fd871c904f508b6d32eb85082ea55c40407
fatima rahimi <iamfatimarahimi@gmail.com>
2023-11-02 17:52:27 +0330

    button recognition type using if

--- Calculater/script.js
+++ Calculater/script.js
@@ -1,5 +1,32 @@
-const input = document.querySelector('input');
-const button = document.querySelectorAll('button');
-button.addEventListener('click',()=>{
-    input.innerHTML = button.values;
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator__keys')
+const key = e.target
+const action = key.dataset.action
+keys.addEventListener('click', e => {
+ if (e.target.matches('button')) {
+   // Do something
+ }
 })
+// Recenising button type using if
+if (!action) {
+  console.log('number key!')
+}
+if (
+  action === 'add' ||
+  action === 'subtract' ||
+  action === 'multiply' ||
+  action === 'divide'
+) {
+  console.log('operator key!')
+}
+if (action === 'decimal') {
+  console.log('decimal key!')
+}
+
+if (action === 'clear') {
+  console.log('clear key!')
+}
+
+if (action === 'calculate') {
+  console.log('equal key!')
+}
