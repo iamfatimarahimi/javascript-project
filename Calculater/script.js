11d3ec5693dd85ea02fb3253754373eede88efea
fatima rahimi <iamfatimarahimi@gmail.com>
2023-11-02 18:20:03 +0330

    Final touches

--- Calculater/script.js
+++ Calculater/script.js
@@ -1,6 +1,22 @@
+const calculate = (n1, operator, n2) => {
+  let result = ''
+  if (operator === 'add') {
+    result = parseFloat(n1) + parseFloat(n2)
+  } else if (operator === 'subtract') {
+    result = parseFloat(n1) - parseFloat(n2)
+  } else if (operator === 'multiply') {
+    result = parseFloat(n1) * parseFloat(n2)
+  } else if (operator === 'divide') {
+    result = parseFloat(n1) / parseFloat(n2)
+  }
+
+  return result
+}
+
 const calculator = document.querySelector('.calculator')
 const display = calculator.querySelector('.calculator__display')
 const keys = calculator.querySelector('.calculator__keys')
+
 keys.addEventListener('click', e => {
   if (e.target.matches('button')) {
     const key = e.target
@@ -8,6 +24,7 @@ keys.addEventListener('click', e => {
     const keyContent = key.textContent
     const displayedNum = display.textContent
     const previousKeyType = calculator.dataset.previousKeyType
+
     Array.from(key.parentNode.children)
       .forEach(k => k.classList.remove('is-depressed'))
 
@@ -17,9 +34,12 @@ keys.addEventListener('click', e => {
       } else {
         display.textContent = displayedNum + keyContent
       }
-      if (action === 'decimal') {
+    }
+
+    if (action === 'decimal') {
       display.textContent = displayedNum + '.'
     }
+
     if (
       action === 'add' ||
       action === 'subtract' ||
@@ -31,7 +51,17 @@ keys.addEventListener('click', e => {
       calculator.dataset.firstValue = displayedNum
       calculator.dataset.operator = action
     }
+
     if (action === 'clear') {
       console.log('clear key!')
     }
-    }}})
+
+    if (action === 'calculate') {
+      const firstValue = calculator.dataset.firstValue
+      const operator = calculator.dataset.operator
+      const secondValue = displayedNum
+
+      display.textContent = calculate(firstValue, operator, secondValue)
+    }
+  }
+})
