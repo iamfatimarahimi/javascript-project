8492d136865a252937e06e2caebcd112d5bbef7f
fatima rahimi <iamfatimarahimi@gmail.com>
2023-11-08 21:34:05 +0330

    spread Operators with objects

--- React/script.js
+++ React/script.js
@@ -3,7 +3,13 @@ function sum(num1,num2,...milad){
 }
 sum(10,20,30,40)
 // spread Operators
-let array1= [1,2,3]
-let array2 = [10,20,30,40]
-let array3= [...array1, ...array2]
-console.log(array3);
+// let array1= [1,2,3]
+// let array2 = [10,20,30,40]
+// let array3= [...array1, ...array2]
+// console.log(array3);
+let object1= {
+    prop: 'value1'
+}
+let object2 = {prop2: 'value2'}
+let object3= {...object1, ...object2}
+console.log(object3);
