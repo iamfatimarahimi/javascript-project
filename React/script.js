617b2e055325415543d717130e7060f7dc955315
fatima rahimi <iamfatimarahimi@gmail.com>
2023-11-08 21:32:10 +0330

    spread Operators with arrays

--- React/script.js
+++ React/script.js
@@ -2,3 +2,8 @@ function sum(num1,num2,...milad){
     console.log(num1,num2,milad);
 }
 sum(10,20,30,40)
+// spread Operators
+let array1= [1,2,3]
+let array2 = [10,20,30,40]
+let array3= [...array1, ...array2]
+console.log(array3);
