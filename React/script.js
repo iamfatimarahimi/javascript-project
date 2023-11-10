e4c3e3829f1b375866ffbf45976f72a7e425f57a
fatima rahimi <iamfatimarahimi@gmail.com>
2023-11-10 19:43:20 +0330

    Filter method in simpler way

--- React/script.js
+++ React/script.js
@@ -37,9 +37,5 @@ let numbers = [1,2,3,4,5,6,7,8,9,10,11]
 // let newNumbers = numbers.map(num => num*10)
 // console.log(newNumbers);
 // Filter method
-let NewNumbers = numbers.filter(function(number){
-    if(number % 2 == 0){
-        return number
-    }
-})
-console.log(NewNumbers);
+let newNumbers =numbers.filter((number)=> number%2 == 0)
+console.log(newNumbers);
